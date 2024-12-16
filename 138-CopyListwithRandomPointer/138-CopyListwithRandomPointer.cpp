e421cabfefada9a22e473bfb3a77302b7475673e
Felipe Martins <6557756+fefas@users.noreply.github.com>
2024-12-16 17:28:09 +0100

    Update file 16/12/2024, 17:28:09

--- 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.cpp
+++ 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.cpp
@@ -16,34 +16,21 @@ public:
 
 class Solution {
 private:
-    unordered_map<Node*,int> original;
-    unordered_map<int,Node*> copied;
-
-    Node* mapList(Node* head, int i = 0)
-    {
-        if (head == NULL)
-            return NULL;
-        
-        Node* copy = new Node(head->val);
-
-        copy->next = mapList(head->next, i + 1);
-
-        original[head] = i;
-        copied[i] = copy;
-
-        return copy;
-    }
+    unordered_map<Node*,Node*> m;
 
 public:
     Node* copyRandomList(Node* head)
     {
-        Node* newHead = mapList(head);
+        for (Node* p = head; p; p = p->next) {
+            m[p] = new Node(p->val);
+        }
 
-        for (Node* p = head, *q = newHead; p; p = p->next, q = q->next) {
-            if (p->random)
-                q->random = copied[original[p->random]];
+        for (Node* p = head; p; p = p->next) {
+            Node* c = m[p];
+            c->next = m[p->next];
+            c->random = p->random == NULL ? NULL : m[p->random];
         }
         
-        return newHead;
+        return m[head];
     }
 };
